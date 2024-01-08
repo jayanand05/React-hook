be054a96635bee071597e9399578c5d583498145
JAY ANAND <jay99628@gmail.com>
2024-01-08 23:49:13 +0530

    Update DataFetching3.js

--- src/components/DataFetching3.js
+++ src/components/DataFetching3.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";rccr
 
 function DataFetching3() {
   const [post, setPost] = useState({});
