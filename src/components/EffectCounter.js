9ed2a36db569ead228e886fb3237cc8c99048406
JAY ANAND <jay99628@gmail.com>
2024-01-09 23:55:50 +0530

    Update EffectCounter.js

--- src/components/EffectCounter.js
+++ src/components/EffectCounter.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";jnjnjn
 
 function EffectCounter() {
   const [count, setCount] = useState(0);
