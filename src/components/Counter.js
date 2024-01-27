5bcfe8e5860544aa0ebfc4fa041fce38a15866b6
JAY ANAND <jay99628@gmail.com>
2024-01-27 23:54:23 +0530

    Update Counter.js

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,4 +1,4 @@
-hjggh import React, { useMemo, useState } from "react";
+import React, { useMemo, useState } from "react";
 
 function Counter() {
   const [counterOne, setCounterOne] = useState(0);
